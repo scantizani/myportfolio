81ba59bc4209a835c748e7e8469f251bb9293a4c
Silvana Rondina <silvanarondina@MacBook-Air-de-Silvana.local>
2025-02-04 01:22:01 -0500

    Fixed GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/myportfolio/',
+  build: {
+    outDir: 'dist',
+  },
 });
